634f38cb3f50dad9a71682dacdada21f47aa843f
Vadasz Laszlo <maxinteger@gmail.com>
2016-06-15 23:35:24 +0100

    add basic mocha setup

---
+++ test/test.js
@@ -0,0 +1,9 @@
+'use strict';
+/* global describe, it */
+let assert = require('assert');
+
+describe('test', function () {
+	it('should not fail', function () {
+		assert.strictEqual(true, true);
+	})
+});
